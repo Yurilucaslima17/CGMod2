2ebe9bf256e68d85a83f98a90a0708f9c2fe3de8
brunoqueiroz1984 <bruno.limapaulista@gmail.com>
2018-06-12 15:19:52 -0300

    modelo atualizado e teclas remapeadas

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -8,11 +8,13 @@ import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.jme3.system.AppSettings;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -23,12 +25,21 @@ public class Main extends SimpleApplication {
     
     private AudioController audioController;
     private Material apertado;
-    private Material mat;
+    private Material mat,mat2;
     private Boolean highpitch = false;
     private Boolean lowpitch = false;
     
     public static void main(String[] args) {
         Main app = new Main();
+        AppSettings a = new AppSettings(true);
+
+        a.setWidth(1280);
+        a.setHeight(720);
+
+        app.setSettings(a);
+        app.setShowSettings(false);
+        app.setDisplayFps(false);
+        app.setDisplayStatView(false);
         app.start();
     }
 
@@ -39,20 +50,34 @@ public class Main extends SimpleApplication {
         mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.White);
         
-        for(int i=0;i<24;i++){
+        for(int i=0;i<28;i++){
             Box b = new Box(1, 1, 3);
             Geometry geom = new Geometry("Box"+i, b);  
             
             geom.setMaterial(mat);
-            geom.setLocalTranslation(i*2.25f, speed, speed);
+            geom.setLocalTranslation(i*2.25f, 0, 0);
             rootNode.attachChild(geom);
+        }
+        
+        mat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat2.setColor("Color", ColorRGBA.Orange);
+        for(int i=0;i<27;i++){
+            if(i!=2&&i!=6&&i!=9&&i!=13&&i!=16&&i!=20&&i!=23&&i!=27){
+                Box b = new Box(1, 1, 2);
+                Geometry geom = new Geometry("Box#"+i, b);  
+
+                geom.setMaterial(mat2);
+                geom.setLocalTranslation(i*2.25f+1, 0.5f, -0.5f);
+                rootNode.attachChild(geom);
+            }
         }
         audioController = new AudioController(rootNode, assetManager);
 
         apertado.setColor("Color", ColorRGBA.Blue);
-        cam.setLocation(new Vector3f(26f, 8f, 42f));
-        //Quaternion q = new Quaternion(2.2217576E-4f, 0.8905794f, -0.45482755f, 4.3457586E-4f);
-        //cam.setRotation(q);
+        cam.setLocation(new Vector3f(30f, 30f, 38f));
+        Quaternion q = new Quaternion();
+        q.fromAngleAxis(-FastMath.HALF_PI/12, Vector3f.UNIT_X);
+        cam.lookAt(rootNode.getChild("Box13").getLocalTranslation(),Vector3f.UNIT_Y);
         initKeys();
         flyCam.setEnabled(false);
     }
@@ -110,135 +135,191 @@ public class Main extends SimpleApplication {
             
             if(name.equals("Do1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box0").setMaterial(apertado);
+                rootNode.getChild("Box7").setMaterial(apertado);
             }
             if(name.equals("Do1") && !isPressed){
-                rootNode.getChild("Box0").setMaterial(mat);
+                rootNode.getChild("Box7").setMaterial(mat);
             }
             
             if(name.equals("Do#1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box1").setMaterial(apertado);
+                rootNode.getChild("Box#7").setMaterial(apertado);
             }
             if(name.equals("Do#1") && !isPressed){
-                rootNode.getChild("Box1").setMaterial(mat);
+                rootNode.getChild("Box#7").setMaterial(mat);
             }
             
             if(name.equals("Re1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box2").setMaterial(apertado);
+                rootNode.getChild("Box8").setMaterial(apertado);
             }
             if(name.equals("Re1") && !isPressed){
-                rootNode.getChild("Box2").setMaterial(mat);
+                rootNode.getChild("Box8").setMaterial(mat);
             }
             
             if(name.equals("Re#1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box3").setMaterial(apertado);
+                rootNode.getChild("Box#8").setMaterial(apertado);
             }
             if(name.equals("Re#1") && !isPressed){
-                rootNode.getChild("Box3").setMaterial(mat);
+                rootNode.getChild("Box#8").setMaterial(mat);
             }
             
             if(name.equals("Mi1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box4").setMaterial(apertado);
+                rootNode.getChild("Box9").setMaterial(apertado);
             }
             if(name.equals("Mi1") && !isPressed){
-                rootNode.getChild("Box4").setMaterial(mat);
+                rootNode.getChild("Box9").setMaterial(mat);
             }
             
             if(name.equals("Fa1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box5").setMaterial(apertado);
+                rootNode.getChild("Box10").setMaterial(apertado);
             }
             if(name.equals("Fa1") && !isPressed){
-                rootNode.getChild("Box5").setMaterial(mat);
+                rootNode.getChild("Box10").setMaterial(mat);
             }
             
             if(name.equals("Fa#1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box6").setMaterial(apertado);
+                rootNode.getChild("Box#10").setMaterial(apertado);
             }
             if(name.equals("Fa#1") && !isPressed){
-                rootNode.getChild("Box6").setMaterial(mat);
+                rootNode.getChild("Box#10").setMaterial(mat);
             }
             
             if(name.equals("Sol1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box7").setMaterial(apertado);
+                rootNode.getChild("Box11").setMaterial(apertado);
             }
             if(name.equals("Sol1") && !isPressed){
-                rootNode.getChild("Box7").setMaterial(mat);
+                rootNode.getChild("Box11").setMaterial(mat);
             }
             
             if(name.equals("Sol#1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box8").setMaterial(apertado);
+                rootNode.getChild("Box#11").setMaterial(apertado);
             }
             if(name.equals("Sol#1") && !isPressed){
-                rootNode.getChild("Box8").setMaterial(mat);
+                rootNode.getChild("Box#11").setMaterial(mat);
             }
             
             if(name.equals("La1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box9").setMaterial(apertado);
+                rootNode.getChild("Box12").setMaterial(apertado);
             }
             if(name.equals("La1") && !isPressed){
-                rootNode.getChild("Box9").setMaterial(mat);
+                rootNode.getChild("Box12").setMaterial(mat);
             }
             
             if(name.equals("La#1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box10").setMaterial(apertado);
+                rootNode.getChild("Box#12").setMaterial(apertado);
             }
             if(name.equals("La#1") && !isPressed){
-                rootNode.getChild("Box10").setMaterial(mat);
+                rootNode.getChild("Box#12").setMaterial(mat);
             }
             
             if(name.equals("Si1") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
-                rootNode.getChild("Box11").setMaterial(apertado);
+                rootNode.getChild("Box13").setMaterial(apertado);
             }
             if(name.equals("Si1") && !isPressed){
-                rootNode.getChild("Box11").setMaterial(mat);
+                rootNode.getChild("Box13").setMaterial(mat);
             }
             
             if(name.equals("Do2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box14").setMaterial(apertado);
+            }
+            if(name.equals("Do2") && !isPressed){
+                rootNode.getChild("Box14").setMaterial(mat);
             }
             if(name.equals("Do#2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box#14").setMaterial(apertado);
+            }
+            if(name.equals("Do#2") && !isPressed){
+                rootNode.getChild("Box#14").setMaterial(mat);
             }
             if(name.equals("Re2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box15").setMaterial(apertado);
+            }
+            if(name.equals("Re2") && !isPressed){
+                rootNode.getChild("Box15").setMaterial(mat);
             }
             if(name.equals("Re#2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box#15").setMaterial(apertado);
             }
+            if(name.equals("Re#2") && !isPressed){
+                rootNode.getChild("Box#15").setMaterial(mat2);
+            }
+            
             if(name.equals("Mi2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box16").setMaterial(apertado);
             }
+            if(name.equals("Mi2") && !isPressed){
+                rootNode.getChild("Box16").setMaterial(mat);
+            }
+            
             if(name.equals("Fa2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box17").setMaterial(apertado);
             }
+            if(name.equals("Fa2") && !isPressed){
+                rootNode.getChild("Box17").setMaterial(mat);
+            }
+            
             if(name.equals("Fa#2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box#17").setMaterial(apertado);
             }
+            if(name.equals("Fa#2") && !isPressed){
+                rootNode.getChild("Box#17").setMaterial(mat2);
+            }
+            
             if(name.equals("Sol2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box18").setMaterial(apertado);
             }
+            if(name.equals("Sol2") && !isPressed){
+                rootNode.getChild("Box18").setMaterial(mat);
+            }
+            
             if(name.equals("Sol#2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box#18").setMaterial(apertado);
             }
+            if(name.equals("Sol#2") && !isPressed){
+                rootNode.getChild("Box#18").setMaterial(mat2);
+            }
+            
             if(name.equals("La2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box19").setMaterial(apertado);
             }
+            if(name.equals("La2") && !isPressed){
+                rootNode.getChild("Box19").setMaterial(mat);
+            }
+            
             if(name.equals("La#2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box#19").setMaterial(apertado);
             }
+            if(name.equals("La#2") && !isPressed){
+                rootNode.getChild("Box#19").setMaterial(mat2);
+            }
+            
             if(name.equals("Si2") && isPressed){
                 audioController.playSound(name, highpitch, lowpitch);
+                rootNode.getChild("Box20").setMaterial(apertado);
+            }
+            if(name.equals("Si2") && !isPressed){
+                rootNode.getChild("Box20").setMaterial(mat);
             }
         }
         
